db60eb62e5afe4d92208b7a05b31a72e26daca4f
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-04-12 16:41:22 +0900

    Add Authentication Logic

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -1,11 +1,31 @@
 package jp.kaleidot725.sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN && resultCode == Activity.RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 100
     }
 }
