6003d5db0b1af25f1c38315546c82287fa3fd024
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-04-15 23:04:06 +0900

    Update Google と Firebase Auth を分けた

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -1,60 +1,89 @@
 package jp.kaleidot725.sample
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 認証状態を取得する
-        val auth  = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        val authorized = (currentUser != null)
+        firebaseAuth = FirebaseAuth.getInstance()
+        googleSignInClient = GoogleSignIn.getClient(
+            this, GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        )
+    }
 
-        // 認証状態に応じて、サインインするかログアウトするか変化させる
-        if (!authorized) {
-            sigh_in_button.text = "Sigh In"
-            sigh_in_button.setOnClickListener { sighIn() }
-        } else {
-            sigh_in_button.text = "Log Out"
-            sigh_in_button.setOnClickListener { logout() }
-        }
+    override fun onStart() {
+        super.onStart()
+        updateUI()
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun signOutGoogle() {
+        googleSignInClient.signOut()
+    }
+
+    private fun signInFirebaseAuth(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    updateUI()
+                }
+            }
+    }
+
+    private fun signOutFirebaseAuth() {
+        firebaseAuth.signOut()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN && resultCode == Activity.RESULT_OK) {
-            // TODO
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                task.getResult(ApiException::class.java)?.let { signInFirebaseAuth(it) }
+            } catch (e: ApiException) {
+                Log.e("MainActivity", "Sign In Google Error")
+            }
         }
     }
 
-    private fun sighIn() {
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        val intent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setLogo(R.drawable.app_icon)
-            .setTheme(R.style.AppTheme)
-            .build()
-
-        startActivityForResult(intent, RC_SIGN_IN)
-    }
-
-    private fun logout() {
-
+    private fun updateUI() {
+        val authorized = (firebaseAuth.currentUser != null)
+        if (!authorized) {
+            sigh_in_button.text = "Sign In"
+            sigh_in_button.setOnClickListener { signInGoogle() }
+        } else {
+            sigh_in_button.text = "Sign Out"
+            sigh_in_button.setOnClickListener {
+                signOutFirebaseAuth()
+                signOutGoogle()
+                updateUI()
+            }
+        }
     }
 
     companion object {
