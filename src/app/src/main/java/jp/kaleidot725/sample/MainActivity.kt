ac6d73a40b290b58ea128337bd504a9d93a40f47
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-04-15 22:19:07 +0900

    Update

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -6,27 +6,27 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private var currentUser: FirebaseUser? = null
-    private var authorized: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sigh_in_button.setOnClickListener { sighIn() }
-    }
 
-    override fun onStart() {
-        super.onStart()
+        // 認証状態を取得する
+        val auth  = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+        val authorized = (currentUser != null)
 
-        auth  = FirebaseAuth.getInstance()
-        currentUser = auth.currentUser
-        authorized = (currentUser != null)
-        sigh_in_status_text_view.text = getStatusStr(authorized)
+        // 認証状態に応じて、サインインするかログアウトするか変化させる
+        if (!authorized) {
+            sigh_in_button.text = "Sigh In"
+            sigh_in_button.setOnClickListener { sighIn() }
+        } else {
+            sigh_in_button.text = "Log Out"
+            sigh_in_button.setOnClickListener { logout() }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -53,7 +53,9 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, RC_SIGN_IN)
     }
 
-    private fun getStatusStr(authorized: Boolean): String = if (authorized) "Authorized" else "Unauthorized"
+    private fun logout() {
+
+    }
 
     companion object {
         private const val RC_SIGN_IN = 100
