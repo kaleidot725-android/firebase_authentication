a7ad7db0fd6372097933e4ba35bc9d60aec3cb0a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-04-17 20:59:20 +0900

    Update

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -9,11 +9,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private val ioScope = CoroutineScope(Dispatchers.IO)
+    private var userToken : String? = null
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
 
@@ -44,50 +51,58 @@ class MainActivity : AppCompatActivity() {
         googleSignInClient.signOut()
     }
 
-    private fun signInFirebaseAuth(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    updateUI()
-                }
-            }
-    }
-
-    private fun signOutFirebaseAuth() {
-        firebaseAuth.signOut()
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
             try {
-                val account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
-                if (account != null) {
-                    signInFirebaseAuth(account)
-                }
+                GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
+                    ?.let {
+                        signInFirebaseAuth(it)
+                    }
             } catch (e: ApiException) {
                 Log.e("MainActivity", "Sign In Google Error")
             }
         }
     }
 
-    private fun updateUI() {
-        val authorized = (firebaseAuth.currentUser != null)
-        if (!authorized) {
-            // SignInGoogle ➔ SignInFirebaseAuth ➔ UpdateUI()
-            sigh_in_button.text = "Sign In"
-            sigh_in_button.setOnClickListener { signInGoogle() }
-        } else {
-            // SignOutFirebaseAuth ➔ SignOutGoogle ➔ Update()
-            sigh_in_button.text = "Sign Out"
-            sigh_in_button.setOnClickListener {
-                signOutFirebaseAuth()
-                signOutGoogle()
+    private fun signInFirebaseAuth(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
                 updateUI()
             }
         }
     }
+
+    private fun signOutFirebaseAuth() {
+        firebaseAuth.signOut()
+    }
+
+    private fun updateUI() {
+        ioScope.launch {
+            if (firebaseAuth.currentUser != null) {
+                userToken = Tasks.await(firebaseAuth.currentUser!!.getIdToken(true)).token
+            }
+
+            withContext(Dispatchers.Main) {
+                if (userToken != null) {
+                    user_id_text_view.text = userToken
+                    sigh_in_button.text = "Sign Out"
+                    sigh_in_button.setOnClickListener {
+                        signOutFirebaseAuth()
+                        userToken = null
+                        signOutGoogle()
+                        updateUI()
+                    }
+                } else {
+                    user_id_text_view.text = userToken
+                    sigh_in_button.text = "Sign In"
+                    sigh_in_button.setOnClickListener { signInGoogle() }
+                }
+            }
+        }
+    }
+
 
     companion object {
         private const val RC_SIGN_IN = 100
